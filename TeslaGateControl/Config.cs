36d63cdd9fb4a11be0c66a7f9f5325a704d7c0e1
Connor Thompson <connorthompson1995@hotmail.com>
2020-07-25 18:35:40 +0100

    Version 1.0.0 Code

---
+++ TeslaGateControl/Config.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled;
+
+namespace TeslaGateControl
+{
+    using Exiled.API.Interfaces;
+    using Log = Exiled.API.Features.Log;
+    public class config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        //Switches between Role determining who avoids the tesla gate and Card determining who avoids the tesla gate
+        public bool cardMode { get; set; } = false;
+        //lists allowed roles that can get through the gate without setting it off. IDs or names can be used 
+        public List<RoleType> allowedRoles { get; set; } = new List<RoleType>();
+        //lists allowed Items that can get through the gate without setting it off. IDs or names can be used
+        public List<ItemType> allowedCards { get; set; } = new List<ItemType>();
+
+        public void ConfigValidator()
+        {
+            foreach(ItemType i in allowedCards)
+            {
+                int id = (int) i;
+                if (id > 7 || id < 0)
+                {
+                    Log.Warn("Only Card IDs can be used here. Restoring Default Config for Allowed Cards");
+                    allowedCards = new List<ItemType>();
+                }
+            }
+        }
+    }
+}
---
+++ TeslaGateControl/TeslaGateControl.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled;
+
+
+namespace TeslaGateControl
+{
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs;
+    using Player = Exiled.Events.Handlers.Player;
+    
+
+    public class TeslaPlugin : Plugin<config>
+    {
+        public eventHandlers eHandler;
+        public bool AllowCards;
+        public List<ItemType> allowedCards;
+        public List<RoleType> allowedRoles;
+        public override void OnEnabled()
+        {
+            AllowCards = Config.cardMode;
+            if (AllowCards)
+            {
+                allowedCards = Config.allowedCards;
+                eHandler = new eventHandlers(allowedCards);
+            }
+            else
+            {
+                allowedRoles = Config.allowedRoles;
+                eHandler = new eventHandlers(allowedRoles);
+            }
+            Player.TriggeringTesla += eHandler.PlayerTesla;
+            Log.Info("Tesla Gate Control V1.0.0 has started Successfully.");
+        }
+
+        public override void OnDisabled()
+        {
+            // Make it dynamically updatable.
+            // We do this by removing the listener for the event and then nulling the event handler.
+            // The more events the more times you have to do this for each one.
+            Player.TriggeringTesla -= eHandler.PlayerTesla;
+            eHandler = null;
+        }
+
+
+    }
+
+    public class eventHandlers
+    {
+        private bool allowCard;
+        private List<ItemType> AllowedTypes;
+        private List<RoleType> allowedRoles;
+        public eventHandlers(List<RoleType> aRoles)
+        {
+            allowedRoles = aRoles;
+            allowCard = false;
+        }
+        public eventHandlers(List<ItemType> aType)
+        {
+            AllowedTypes = aType;
+            allowCard = true;
+        }
+
+        public void PlayerTesla(TriggeringTeslaEventArgs ev)
+        {
+            if (allowCard)
+            {
+                bool validCard = false;
+                var playerInventory = ev.Player.Inventory.items;
+                if (playerInventory != null)
+                {
+                    foreach (var item in playerInventory)
+                    {
+                        if (AllowedTypes.Count > 0 && !AllowedTypes.Contains(item.id))
+                            continue;
+                        else if (AllowedTypes.Contains(item.id))
+                        {
+                            validCard = true;
+                            break;
+                        }
+                           
+                    }
+                }
+                if (validCard)
+                {
+                    ev.IsTriggerable = false;
+                }
+            }
+
+
+            if (allowedRoles != null)
+            {
+                foreach (RoleType role in allowedRoles)
+                {
+                    if (ev.Player.Role == role)
+                    {
+                        ev.IsTriggerable = false;
+                    }
+                }
+            }
+
+        }
+        
+    }
+
+}
